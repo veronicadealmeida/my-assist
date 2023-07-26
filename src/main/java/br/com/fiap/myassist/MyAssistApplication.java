3c596f1beee109d6e5e9e06fdc9f372d35c376f4
Veronica <almeida.vrc@gmail.com>
2023-07-26 20:27:11 -0300

    Firs commit with Spring Initializr

---
+++ src/main/java/br/com/fiap/myassist/MyAssistApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.myassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAssistApplication.class, args);
+	}
+
+}
