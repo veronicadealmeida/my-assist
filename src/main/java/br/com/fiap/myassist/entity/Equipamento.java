8c45e19b6ebbebc5ecbd3cbc779f634e3dcaefe9
Veronica <almeida.vrc@gmail.com>
2023-07-28 20:19:22 -0300

    Add database connection to TBL EQUIPMENT table

--- src/main/java/br/com/fiap/myassist/entity/Equipamento.java
+++ src/main/java/br/com/fiap/myassist/entity/Equipamento.java
@@ -1,15 +1,30 @@
 package br.com.fiap.myassist.entity;
 
 import br.com.fiap.myassist.enums.TipoEquipamentoEnum;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "TBL_EQUIPAMENTO")
 public class Equipamento {
+    @Id
+    @Column(name = "ID_EQUIPAMENTO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "TX_MARCA")
     private String marca;
+
+    @Column(name = "TX_MODELO")
     private String modelo;
+
+    @Column(name = "TX_NUMERO_SERIE")
     private String numeroSerie;
+
+    @Column(name = "TX_TIPO")
+    @Enumerated(EnumType.STRING)
     private TipoEquipamentoEnum tipo;
 }
